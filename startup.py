1f7882e82cf13f1572439bb4168e5dd0f8fb272c
endogen <081081@gmx.net>
2022-04-25 03:32:34 +0200

    Reworked sync_blocks method

--- startup.py
+++ startup.py
@@ -1,24 +1,19 @@
 import gc
 import os
-import pathlib
-import string
-import random
 import time
-
 import rel
 import json
 import websocket
 import requests as r
 
+from pathlib import Path
 from config import Config
 from threading import Thread
 from loguru import logger
 from timeit import default_timer as timer
 
 
-# TODO: Is 'block_latest' even needed?
 # TODO: Erzeuge events für diverse Ereignisse
-# TODO: Zeitmessungen für diverse calls einbauen für debug
 # TODO: Global state zusammenbauen
 # TODO: Endpunkte ähnlich wie bei BlockService
 # TODO: Import von Blocks erlauben
@@ -56,99 +51,101 @@ class BlockGrabber:
         return event['event'], event['data']
 
     def on_message(self, ws, message):
-        logger.debug(message)
+        logger.debug(f'New event --> {message}')
 
         event, block = self.decode_event(message)
-        block_latest = block['number']
-
-        self.cfg.set('block_latest', block_latest)
 
         if event == 'latest_block':
-            if self.cfg.get('block_current') != block_latest:
-                Thread(self.sync_blocks())
-
+            self.cfg.set('block_latest', block['number'])
         elif event == 'new_block':
-            Thread(self.sync_blocks())
+            Thread(target=self.process_block, args=block)
 
     def on_error(self, ws, error):
         logger.debug(error)
 
     def on_close(self, ws, close_status_code, close_msg):
         logger.debug("Websocket connection closed")
-        # TODO: Reconnect
 
     def on_open(self, ws):
         logger.debug("Opened websocket connection")
 
-    def save_block_in_db(self, block_num: int, content: dict):
-        # TODO
+    def process_block(self, content: dict):
+        start_time = timer()
+
+        self.save_block_in_db(content)
+
+        if self.cfg.get('save_to_dir'):
+            self.save_block_in_file(content)
+
+        logger.debug(f'Processing block {content["number"]} took {timer() - start_time}')
+
+    def save_block_in_db(self, content: dict):
+        # TODO: Create new DB connection each time to be thread safe
         pass
 
-    def save_block_in_file(self, block_num: int, content: dict):
-        folder = self.cfg.get('save_blocks_to')
+    def save_block_in_file(self, content: dict):
+        block_dir = self.cfg.get('save_to_dir')
+        block_num = content['number']
 
-        if not folder: return
-
-        file = os.path.join(folder, f'{block_num}.json')
+        file = os.path.join(block_dir, f'{block_num}.json')
         os.makedirs(os.path.dirname(file), exist_ok=True)
-        with open(file, 'w', encoding='utf-8') as file_obj:
-            json.dump(content, file_obj, ensure_ascii=False, sort_keys=True, indent=4)
+
+        with open(file, 'w', encoding='utf-8') as f:
+            json.dump(content, f, sort_keys=True, indent=4)
             logger.debug(f'Saved block {block_num} to file')
 
-    # TODO: Create periodic job that processes blocks on list 'blocks_to_sync'
-    # TODO: Execute periodic job only if 'sync_blocks' doesn't run
-    # TODO: Periodic job can only run if it isn't already running
+    # TODO: Create periodic job that checks blocks between 'block_current' and 'block_latest'
+    # TODO: Process only if websocket connected
+    # TODO: Allow only one instance
     def sync_blocks(self, start: int = None, end: int = None):
         start_time = timer()
-        uid = self._id()
 
         start = start if start else self.cfg.get('block_current')
         end = end if end else self.cfg.get('block_latest')
-
-        self.cfg.set('block_current', self.cfg.get('block_latest'))
 
         to_sync = list(range(start + 1, end + 1))
-        syncing = self.cfg.get('blocks_to_sync')
-        do_sync = [x for x in to_sync if x not in syncing]
+        logger.debug(f'Blocks to sync: {to_sync}')
 
-        logger.debug(f'{uid} TO_SYNC: {to_sync}')
-        logger.debug(f'{uid} SYNCING: {syncing}')
-        logger.debug(f'{uid} DO_SYNC: {do_sync}')
+        missing = self.cfg.get('missing_blocks')
+        logger.debug(f'Missing blocks: {missing}')
+        self.cfg.set('missing_blocks', list())
 
-        syncing.extend(do_sync)
-        syncing.sort(key=int)
-        self.cfg.set('blocks_to_sync', syncing)
+        to_sync.extend(missing)
+        to_sync.sort(key=int)
+        missing = list()
 
-        logger.debug(f'{uid} SYNCING: {syncing}')
+        block_dir = self.cfg.get('save_to_dir')
+        sleep_for = self.cfg.get('block_sync_wait')
 
-        done_sync = list()
-        for block_num in do_sync:
-            block = self.get_block(block_num)
+        for block_num in to_sync:
+            logger.debug(f'Checking block {block_num}...')
 
-            self.save_block_in_db(block_num, block)
-            self.save_block_in_file(block_num, block)
+            # TODO: Check block data in DB
+            #  If not present: self.save_block_in_db()
 
-            done_sync.append(block_num)
+            if block_dir:
+                if not Path(os.path.join(block_dir, f'{block_num}.json')).is_file():
+                    logger.warning(f'No file for block {block_num} in {block_dir}')
 
-        syncing = self.cfg.get('blocks_to_sync')
-        todo = [x for x in syncing if x not in done_sync]
-        self.cfg.set('blocks_to_sync', todo)
+                    time.sleep(sleep_for)
+                    block = self.get_block(block_num)
 
-        logger.debug(f'{uid} SYNCING: {todo}')
-        logger.debug(f'{uid} Sync time: {timer() - start_time}')
+                    if not block:
+                        missing.append(block_num)
+                        continue
+                    if 'error' in block:
+                        continue
 
-    """
-    def get_blocks(self, start: int = None, end: int = None):
-        start = start if start else self.cfg.get('block_current')
-        end = end if end else self.cfg.get('block_latest')
+                    self.process_block(block)
 
-        while start <= end:
-            self.get_block(start)
-            start += 1
-    """
+        self.cfg.set('block_current', end)
+        self.cfg.set('missing_blocks', missing)
+
+        logger.warning(f'Missing blocks: {missing}')
+        logger.debug(f'Syncing blocks took {timer() - start_time} seconds')
 
     def get_block(self, block_num: int):
-        source = self.cfg.get('url_block_service')
+        source = self.cfg.get('url_blockservice')
         if source:
             source += f'/blocks/{block_num}'
         else:
@@ -174,22 +171,6 @@ class BlockGrabber:
         except Exception as e:
             logger.exception(f'get_block({block_num}) --> {e}')
             return None
-
-    def check_block_files(self):
-        folder = '/Users/endogen/Projekte/lamden-blocks'
-
-        for i in range(1, self.cfg.get('block_latest') + 1):
-            if not pathlib.Path(os.path.join(folder, f'{i}.json')).is_file():
-                print("MISSING BLOCK", i)
-                self.get_block(i)
-
-    def panic(self, message):
-        logger.error(f'PANIC: {message}')
-        # TODO: Notify
-
-    def _id(self, length: int = 8):
-        alphabet = string.ascii_uppercase + string.digits
-        return ''.join(random.choices(alphabet, k=length))
 
 
 if __name__ == "__main__":
