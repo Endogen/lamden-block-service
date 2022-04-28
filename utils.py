26c429aef5cb889b1d9130613ae35705a518340a
endogen <081081@gmx.net>
2022-04-28 08:34:03 +0200

    Added (empty) CLI module for utilities

---
+++ utils.py
@@ -0,0 +1,7 @@
+# CLI with Typer to
+# - Sync single block
+# - Range of blocks
+# - Download all blocks from GitHub
+# - Sync old blocks (after download)
+# - Check if all blocks in DB
+# - Check if all blocks on HDD
