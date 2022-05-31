f1323f1a64faccfedfb8acb827322e1e8759413d
endogen <081081@gmx.net>
2022-06-01 01:13:26 +0200

    Started implementing APi

--- api.py
+++ api.py
@@ -2,6 +2,7 @@ import os
 import sys
 import uvicorn
 import utils
+import sql
 
 from pathlib import Path
 from config import Config
@@ -11,7 +12,10 @@ from fastapi import FastAPI
 from datetime import timedelta
 from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
+from timeit import default_timer as timer
 
+# TODO: Search contract code contains
+# TODO: Total burned amount for token
 # TODO: API for total stamps used for address
 # TODO: API for 'network involvement' of an address - how much other addresses transacted with address?
 # TODO: Use similar API as default Block Service
@@ -32,7 +36,7 @@ app.add_middleware(
     allow_credentials=True
 )
 
-cfg = Config(os.path.join('cfg', 'config.json'))
+cfg = Config(os.path.join('cfg', 'api.json'))
 db = DB(cfg)
 
 logger.remove()
@@ -58,24 +62,26 @@ def root():
 
 @app.get("/db-size")
 def db_size():
-    logger.debug(f'API ENTRY: db_size()')
+    start = timer()
 
-    size = db.execute('size_select', {'db': cfg.get('db_name')})
-    logger.debug(f'API RESULT: {size}')
-
-    return size[0][0]
+    try:
+        logger.debug(f'API ENTRY: db_size()')
+        result = db.execute_raw(sql.db_size(cfg.get('db_name')))
+        logger.debug(f'API RESULT after {timer() - start:.4f} seconds: {result}')
+        return result[0][1]
+    except Exception as e:
+        return {'error': str(e)}
 
 
 # TODO: Split into 'addresses' and 'contracts' and sort by balance and show balance
 @app.get("/holders/{contract}")
-def holders(contract: str):
-    logger.debug(f'API ENTRY: holders({contract})')
-
-    holders = db.execute('holders_select', {'l': f'{contract}.balances%'})
-    logger.debug(f'API RESULT: {holders}')
+def holders(contract: str, addresses: bool = True, contracts: bool = True, top: int = 0):
+    logger.debug(f'API ENTRY: holders({contract}, {addresses}, {contracts}, {top})')
+    result = db.execute_raw(sql)
+    logger.debug(f'API RESULT: {result}')
 
     holder_list = list()
-    for holder in holders:
+    for holder in result:
         holder_split = holder[0].split(':')
         if len(holder_split) == 2:
             holder_list.append(holder_split[1])
@@ -118,11 +124,27 @@ def state(contract: str = None):
     if contract:
         result = db.execute('state_contract_select', {'l': f'{contract}'})
         logger.debug(f'API RESULT: {result}')
-
+        # TODO
 
     else:
         result = db.execute('state_select')
         logger.debug(f'API RESULT: {result}')
+
+
+@app.get("/contract/{contract}")
+def contract(contract: str):
+    logger.debug(f'API ENTRY: state({contract})')
+    result = db.execute('contract_select', {'c': f'{contract}'})
+    logger.debug(f'API RESULT: {result}')
+    return result[0][0]
+
+
+@app.get("/contracts")
+def contracts(contract: str = None, lst001: str = None, lst002: str = None):
+    logger.debug(f'API ENTRY: state({contract}, {lst001}, {lst002})')
+    result = db.execute('contracts_select')
+    logger.debug(f'API RESULT: {result}')
+    return result
 
 
 uvicorn.run(app, host=cfg.get('api_host'), port=cfg.get('api_port'))
